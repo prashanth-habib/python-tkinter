8041e49afc30b08716d0f4c987ebb85c32050e65
prashanth <prashanthe3132@gmail.com>
2023-11-14 23:54:21 +0530

    added radio button file

--- radioButton.py
+++ radioButton.py
@@ -1,22 +1,32 @@
 import tkinter as tk
+from functools import partial
 
 Pizza_list = ["Mexican Green Wave", "Indi Tandoor Paneer", "Veg Loaded"]
-options = ["Eat-in", "Take-away"]
+
+def order(your_order: str):
+    print(f"Your order is {your_order}")
+    # if x.get() == 0:
+    #     print(f"Placing order for {Pizza_list[0]}")
+    # elif x.get() == 1:
+    #     print(f"Placing order for {Pizza_list[1]}")
+    # elif x.get() == 2:
+    #     print(f"Placing order for {Pizza_list[2]}")
 
 main_window = tk.Tk()
 main_window.title("Pizzania")
 
 x = tk.IntVar()
-for pizza in Pizza_list:
+for pizza_index in range(len(Pizza_list)):
     radioButton = tk.Radiobutton(main_window,
-                                 text=pizza,
+                                 text=Pizza_list[pizza_index],
                                  variable=x,
-                                 value=pizza,
+                                 value=pizza_index,
                                  padx=15,
                                  pady=10,
                                  font=("Consolas", 20),
-                                 indicatoron=0,
-                                 width=20)
-    radioButton.pack(anchor=tk.W)
+                                 indicatoron=False,
+                                 width=20,
+                                 command=partial(order, Pizza_list[pizza_index]))
+    radioButton.pack()
 
 main_window.mainloop()
