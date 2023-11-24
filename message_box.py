410583acf8bb3b474c90b45ad8bb39624ca2d1b7
p.habib.eshwar <p.habib.eshwar@accenture.com>
2023-11-24 21:41:26 +0530

    added message box code

---
+++ message_box.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def open_new_window():
+    print("Opening new window")
+    messagebox.showwarning(title="Bot Check", message="You are not a robot!")
+
+main_window = tk.Tk()
+main_window.title("home")
+
+main_button = tk.Button(
+    main_window,
+    text="open window",
+    command=open_new_window
+)
+main_button.pack()
+main_window.mainloop()
