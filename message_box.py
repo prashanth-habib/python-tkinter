3f7fc97fbc6112d71707bfd5e702e6f03418f22f
prashanth <prashanthe3132@gmail.com>
2023-11-25 22:52:29 +0530

    updated message box code

--- message_box.py
+++ message_box.py
@@ -3,7 +3,11 @@ from tkinter import messagebox
 
 def open_new_window():
     print("Opening new window")
-    messagebox.showwarning(title="Bot Check", message="You are not a robot!")
+    # messagebox.showwarning(title="Bot Check", message="You are not a robot!")
+    if messagebox.askyesno(title="check", message="are you a robot?"):
+        print("You are a robot!")
+    else:
+        print("You are not a robot!!")
 
 main_window = tk.Tk()
 main_window.title("home")
